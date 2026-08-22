06260988b4934a32f5b208ba461f035561c70733
aleksi <aleksisiniauski@gmail.com>
2026-08-22 10:30:14 +0200

    Initial Mini Sportsbook project

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Mini Sportsbook");
+        System.out.println();
+        System.out.println("Let's make it");
+    }
+}
